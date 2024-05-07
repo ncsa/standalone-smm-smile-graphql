b7277893b7808b4cf948203cadd7fa7fc64cf3e1
Chen Wang <cwang138@illinois.edu>
2024-05-07 09:32:12 -0500

    search endpoint ready for youtube

--- API/youtubeAPI.js
+++ API/youtubeAPI.js
@@ -25,20 +25,43 @@ function youtubeAPI(resolveName, id, args){
         switch(resolveName){
             case 'search':
                 youtube.search.list({
-                    part: 				'id,snippet',
-                    key: 				config.youtube.api_key,
-                    maxResults:			args['maxResults'],
-                    order:				args['order'],
-                    publishedAfter:		args['publishedAfter'],
-                    publishedBefore:	args['publishedBefore'],
-                    q: 					args['q'],
-                    type:				args['type'],
+                    forContentOwner:    args['forContentOwner'],
+                    forDeveloper:       args['forDeveloper'],
+                    forMine:            args['forMine'],
+                    part: 				args['part'] ? args['part']: 'id,snippet',
+                    channelId:			args['channelId'],
+                    channelType:		args['channelType'] ? args['channelType'] : 'any',
+                    eventType:			args['eventType'], // completed, live, upcoming
+                    location:			args['location'], // (37.42307,-122.08427)
+                    locationRadius:		args['locationRadius'], // 1500m, 5km, 10000ft, 0.75mi
+                    order:				args['order'], // date, rating, relevance, title, videoCount, viewCount
+                    maxResults:			args['maxResults'], // 0 to 50
+                    onBehalfOfContentOwner: args['onBehalfOfContentOwner'], // string
+                    pageToken:			args['pageToken'], // string
+                    publishedAfter:		args['publishedAfter'], // 1970-01-01T00:00:00Z
+                    publishedBefore:	args['publishedBefore'], // 1970-01-01T00:00:00Z
+                    q: 					args['q'], // query term boolean NOT (-) OR (|)
+                    regionCode:			args['regionCode'], // ISO 3166-1 alpha-2
+                    relevanceLanguage:	args['relevanceLanguage'], // ISO 639-1
+                    safeSearch:			args['safeSearch'], // moderate, none, strict
+                    topicId:			args['topicId'], // string
+                    type:				args['type'], // channel, playlist, video
+                    videoCaption:		args['videoCaption'], // any, closedCaption, none
+                    videoCategoryId:	args['videoCategoryId'], // string
+                    videoDefinition:	args['videoDefinition'], // any, high, standard
+                    videoDimension:		args['videoDimension'], // 2d, 3d, any
+                    videoDuration:		args['videoDuration'], // any, long, medium, short
+                    videoEmbeddable:	args['videoEmbeddable'], // any, true
+                    videoLicense:		args['videoLicense'], // any, creativeCommon, youtube
+                    videoSyndicated:	args['videoSyndicated'], // any, true
+                    videoSyndicationType:args['videoSyndicationType'], // any, broadcast, none
+                    videoType:			args['videoType'], // any, episode, movie
                 }, (error, data) => {
                     if (error){
                         console.error(error);
                         reject(error);
                     }else{
-                        resolve(data.items);
+                        resolve(data.data.items);
                     }
                 });
                 break;
--- data/schema/youtube-type/youtubeListType.js
+++ data/schema/youtube-type/youtubeListType.js
@@ -1,62 +1,108 @@
 var {
-	GraphQLSchema,
-	GraphQLObjectType,
-	GraphQLString,
-	GraphQLList,
-	GraphQLInt,
-	GraphQLFloat,
-	GraphQLBoolean
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLFloat,
+    GraphQLBoolean
 } = require('graphql');
 var youtubeAPI = require('../../../API/youtubeAPI');
 
 const youtubeListType = module.exports = new GraphQLObjectType({
-	name:'youtubeList',
-	description:'',
-	fields:() => ({
-		kind:		{type:GraphQLString},
-		etag:		{type:GraphQLString},
-		info:		{type:youtubeInfoType,
-						resolve: ({id})=>{return id}},
-		snippet:	{type:youtubeSnippetType},
-	})
+    name: 'youtubeList',
+    description: '',
+    fields: () => ({
+        kind: {type: GraphQLString},
+        etag: {type: GraphQLString},
+        id: {type: youtubeInfoType},
+        snippet: {type: youtubeSnippetType},
+    })
 });
 
 const youtubeInfoType = new GraphQLObjectType({
-	name:'youtubeId',
-	fields:() =>({
-		kind:		{type:GraphQLString},
-		videoId:	{type:GraphQLString},
-		channelId:	{type:GraphQLString},
-		playlistId:	{type:GraphQLString},
-		/*---------------nested--------------*/
-		videoInfo:		{type:new GraphQLList(youtubeVideoType),
-							resolve:({videoId}) => youtubeAPI(resolveName='video',id=videoId, args={})},
-		channelInfo:	{type:new GraphQLList(youtubeChannelType),
-							resolve:({channelId}) => youtubeAPI(resolveName='channel',id=channelId, args={})},
-		playlistInfo:	{type:new GraphQLList(youtubePlaylistType),
-							resolve: ({playlistId}) => youtubeAPI(resolveName='playlist',id=playlistId, args={})},	
-	})
+    name: 'youtubeId',
+    fields: () => ({
+        kind: {type: GraphQLString},
+        videoId: {type: GraphQLString},
+        channelId: {type: GraphQLString},
+        playlistId: {type: GraphQLString},
+        // TODO investigate if those work or not later
+        // /*---------------nested--------------*/
+        // videoInfo:		{type:new GraphQLList(youtubeVideoType),
+        // 					resolve:({videoId}) => youtubeAPI(resolveName='video',id=videoId, args={})},
+        // channelInfo:	{type:new GraphQLList(youtubeChannelType),
+        // 					resolve:({channelId}) => youtubeAPI(resolveName='channel',id=channelId, args={})},
+        // playlistInfo:	{type:new GraphQLList(youtubePlaylistType),
+        // 					resolve: ({playlistId}) => youtubeAPI(resolveName='playlist',id=playlistId, args={})},
+    })
 });
 
 const youtubeSnippetType = new GraphQLObjectType({
-	name:'youtubeSnippet',
-	fields: ()=> ({
-		publishedAt:			{type:GraphQLString},
-		channelId:				{type:GraphQLString},
-		title:					{type:GraphQLString},
-		description:			{type:GraphQLString},
-		default_thumbnails:		{type:youtubeThumbnailType,
-									resolve:({thumbnails})=>{return thumbnails.default}},
-		medium_thumbnails:		{type:youtubeThumbnailType,
-									resolve:({thumbnails})=>{return thumbnails.medium}},
-		high_thumbnails:		{type:youtubeThumbnailType,
-									resolve:({thumbnails})=>{return thumbnails.high}},
-		channelTitle:			{type:GraphQLString},
-		liveBroadcastContent:	{type:GraphQLString},
-	})
+    name: 'youtubeSnippet',
+    fields: () => ({
+        publishedAt: {type: GraphQLString},
+        channelId: {type: GraphQLString},
+        title: {type: GraphQLString},
+        description: {type: GraphQLString},
+        default_thumbnails_url: {
+            type: GraphQLString,
+            resolve: ({thumbnails}) => {return thumbnails.default.url}
+        },
+        default_thumbnails_width: {
+            type: GraphQLInt,
+            resolve: ({thumbnails}) => {return thumbnails.default.width}
+        },
+        default_thumbnails_height: {
+            type: GraphQLInt,
+            resolve: ({thumbnails}) => {return thumbnails.default.height}
+        },
+        medium_thumbnails_url: {
+            type: GraphQLString,
+            resolve: ({thumbnails}) => {return thumbnails.medium.url}
+        },
+        medium_thumbnails_width: {
+            type: GraphQLInt,
+            resolve: ({thumbnails}) => {return thumbnails.medium.width}
+        },
+        medium_thumbnails_height: {
+            type: GraphQLInt,
+            resolve: ({thumbnails}) => {return thumbnails.medium.height}
+        },
+        high_thumbnails_url: {
+            type: GraphQLString,
+            resolve: ({thumbnails}) => {return thumbnails.high.url}
+        },
+        high_thumbnails_width: {
+            type: GraphQLInt,
+            resolve: ({thumbnails}) => {return thumbnails.high.width}
+        },
+        high_thumbnails_height: {
+            type: GraphQLInt,
+            resolve: ({thumbnails}) => {return thumbnails.high.height}
+        },
+        standard_thumbnails_url: {
+            type: GraphQLString,
+            resolve: ({thumbnails}) => {return thumbnails.standard.url}
+        },
+        standard_thumbnails_width: {
+            type: GraphQLInt,
+            resolve: ({thumbnails}) => {return thumbnails.standard.width}
+        },
+        standard_thumbnails_height: {
+            type: GraphQLInt,
+            resolve: ({thumbnails}) => {return thumbnails.standard.height}
+        },
+        maxres_thumbnails_url: {
+            type: GraphQLString,
+            resolve: ({thumbnails}) => {return thumbnails.maxres.url}
+        },
+        channelTitle: {type: GraphQLString},
+        liveBroadcastContent: {type: GraphQLString},
+    })
 });
 
-const youtubeThumbnailType = require('./youtubeThumbnailType');
-const youtubePlaylistType = require('./youtubePlaylistType');
-const youtubeChannelType = require('./youtubeChannelType');
-const youtubeVideoType = require('./youtubeVideoType');
+// const youtubeThumbnailType = require('./youtubeThumbnailType');
+// const youtubePlaylistType = require('./youtubePlaylistType');
+// const youtubeChannelType = require('./youtubeChannelType');
+// const youtubeVideoType = require('./youtubeVideoType');
